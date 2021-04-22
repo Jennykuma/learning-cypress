7ae529b0e7f67ec8d85d4de7bb5c24a7ce0121f0
Jenny <jenny.le@ucalgary.ca>
2021-04-21 18:12:18 -0600

    Cypress built-in plugin to generate locators

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,10 @@
+/// <reference types=""
+
 describe('My First Test Suite', function() {
 
   it('My First Test Case', function() {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/") // Visit any website
+    cy.get('.search-keyword').type('ca')
   })
 
   it('My Second Test Case', function() {
