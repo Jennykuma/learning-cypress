af447f1778de5bbdec8f2a6fa614e8e5681ad6bf
Jenny <jenny.le@ucalgary.ca>
2021-04-22 18:46:59 -0600

    Basic Assertion in writing the tests with Cypress

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,13 +1,12 @@
-/// <reference types=""
+/// <reference types="Cypress" />
 
 describe('My First Test Suite', function() {
 
   it('My First Test Case', function() {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/") // Visit any website
     cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.product').should('have.length', 4)
   })
 
-  it('My Second Test Case', function() {
-
-  })
 })
